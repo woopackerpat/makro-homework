7483695634a0397c286d9c7f6d18fe4e1c96a272
woopackerpat <p.taechakraichana@gmail.com>
2024-01-23 09:29:59 +0700

    middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./middlewares/error");
+const notFoundHandler = require("./middlewares/notFound");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(errorHandler);
+app.use("*", notFoundHandler);
 
 const port = process.env.PORT;
 app.listen(port, () => {
